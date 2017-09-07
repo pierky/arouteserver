79d7853020338ef67d0fe25692e762ee8006cafd
Pier Carlo Chiodi <pierky@pierky.com>
2017-09-07 18:38:03 +0200

    v0.10.0

--- pierky/arouteserver/version.py
+++ pierky/arouteserver/version.py
@@ -13,5 +13,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.9.3"
+__version__ = "0.10.0"
 COPYRIGHT_YEAR = 2017
