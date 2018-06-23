4452828195291fd577c4d991294d170f64e4d37a
Pier Carlo Chiodi <pierky@pierky.com>
2018-06-23 12:45:45 +0000

    restore support for OpenBSD 6.0

--- pierky/arouteserver/builder.py
+++ pierky/arouteserver/builder.py
@@ -731,7 +731,7 @@ class OpenBGPDConfigBuilder(ConfigBuilder):
                        "footer"]
     LOCAL_FILES_BASE_DIR = "/etc/bgpd"
 
-    AVAILABLE_VERSION = ["6.1", "6.2", "6.3", "6.4"]
+    AVAILABLE_VERSION = ["6.0", "6.1", "6.2", "6.3", "6.4"]
     DEFAULT_VERSION = "6.3"
 
     IGNORABLE_ISSUES = ["path_hiding", "transit_free_action",
