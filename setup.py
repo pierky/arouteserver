ac2dcd3ee20b684f1b294cee5dfe901bce5700ef
Pier Carlo Chiodi <pierky@pierky.com>
2022-01-23 15:56:21 +0100

    Extend to other versions of Python

--- setup.py
+++ setup.py
@@ -124,6 +124,8 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
 
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: System :: Networking",
