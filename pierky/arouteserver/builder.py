a363ad5b74ff8c9b65cc97d1553076ac14537e42
Pier Carlo Chiodi <pierky@pierky.com>
2023-05-20 10:04:25 +0200

    OpenBGPD 8.0

--- pierky/arouteserver/builder.py
+++ pierky/arouteserver/builder.py
@@ -1051,7 +1051,7 @@ class OpenBGPDConfigBuilder(ConfigBuilder):
     LOCAL_FILES_BASE_DIR = "/etc/bgpd"
 
     AVAILABLE_VERSION = ["7.0", "7.1", "7.2", "7.3", "7.4", "7.5", "7.6", "7.7",
-                         "7.8"]
+                         "7.8", "8.0"]
     DEFAULT_VERSION = AVAILABLE_VERSION[-1]
 
     IGNORABLE_ISSUES = ConfigBuilder.IGNORABLE_ISSUES + \
