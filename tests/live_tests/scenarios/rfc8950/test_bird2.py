c4beac6a0014ef1ec021ec2de35eff3bbb9257ca
Pier Carlo Chiodi <pierky@pierky.com>
2024-03-11 21:05:07 +0100

    Add tests for RFC8950

---
+++ tests/live_tests/scenarios/rfc8950/test_bird2.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2017-2024 Pier Carlo Chiodi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from .base import RFC8950Scenario
+from pierky.arouteserver.tests.live_tests.bird import BIRD2Instance
+
+
+class RFC8950Scenario_BIRD2IPv6(RFC8950Scenario):
+
+    __test__ = True
+
+    SHORT_DESCR = "Live test, BIRD v2, RFC8950"
+    RS_INSTANCE_CLASS = BIRD2Instance
+    CLIENT_INSTANCE_CLASS = BIRD2Instance
